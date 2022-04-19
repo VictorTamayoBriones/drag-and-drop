e2436a3f018810fe5d4e56fc79b86896c29ea3e8
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-04-19 16:59:50 -0500

    add localStorage

--- js/main.js
+++ js/main.js
@@ -3,5 +3,24 @@ const list = document.querySelector('#lista');
 Sortable.create(list,{
     animation: 150,
     chosenClass: "selected",
-    ghostClass: "ghost"
+    ghostClass: "ghost",
+    // dragClass: "drag"
+
+    onEnd: () => {
+        console.log('Se incerto un elemento')
+    },
+    group: "lista-personas",
+    store: {
+        //Guardamos el ordend e la lista
+        set: (sortable)=>{
+            const orden = sortable.toArray();
+            localStorage.setItem(sortable.options.group.name, orden.join('-'))
+        },
+
+        //Obtenemos el orden de la lista
+        get: (sortable)=>{
+            const orden = localStorage.getItem(sortable.options.group.name)
+            return orden ? orden.split('-') : [];
+        }
+    }
 });
